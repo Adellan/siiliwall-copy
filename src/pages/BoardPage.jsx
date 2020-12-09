2f0824586d171b2a63e1a33e8016178846e8663a
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-12-09 16:02:23 +0200

    Refactored the filtering function to it's own module in utils folder.

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import {
     Grid,
-    InputLabel,
     MenuItem,
     FormControl,
     Select,
@@ -14,6 +13,7 @@ import useBoardById from '../graphql/board/hooks/useBoardById'
 import useBoardSubscriptions from '../graphql/subscriptions/useBoardSubscriptions'
 import { client } from '../apollo'
 import Header from '../components/utils/Header'
+import { filterBoardByOption } from '../utils/boardFiltering'
 
 const BoardPage = ({ id, eventId }) => {
     useEffect(() => () => {
@@ -34,7 +34,6 @@ const BoardPage = ({ id, eventId }) => {
     const board = queryResult.data.boardById
     const { users } = board
     const userNames = users.map(user => user.userName)
-    const boardsTickets = board.columns.map(column => column.tasks.concat(column.subtasks)).flat()
 
     const switchView = (viewParam) => {
         toggleView(viewParam)
@@ -63,38 +62,6 @@ const BoardPage = ({ id, eventId }) => {
     const closeOptionSelector = () => {
         setOptionSelector(false)
     }
-    console.log(board)
-    const filterBoardByOption = () => {
-        setFilteredBoard(board)
-        let filteredTicketList = []
-        // We go through the array of tickets in the board and push tickets which owner or member the selected user is
-        boardsTickets.map(ticket => {
-            if (ticket.owner && ticket.owner.userName === selectedUser) {
-                filteredTicketList.push(ticket)
-            } else if (ticket.members && ticket.members.find(member => member.userName === selectedUser)) {
-                filteredTicketList.push(ticket)
-            }
-        })
-        // We create an array of all the boards ticketOrderObjects
-        const ticketOrderOfBoard = board.columns.map(column => column.ticketOrder).flat()
-        // We create an array where we include only the ticketOrderObjects that belong to the selected user
-        const filteredTicketOrder = filteredTicketList.map(ticket => ticketOrderOfBoard.find(ticketOrderObj => ticketOrderObj.ticketId === ticket.id))
-        // We go through the board's columns and create a new array of their ticketOrders where we only show tickets belonging to the selected user
-        const newTicketOrdersForColumns = board.columns.map(column => column.ticketOrder).map(colTicketOrderArr =>
-            colTicketOrderArr.filter(colTicketOrderObj =>
-                filteredTicketOrder.find(ticketOrderObj => ticketOrderObj.ticketId === colTicketOrderObj.ticketId)))
-        // We create a copy of boards columns with filtered ticketOrders
-        const filteredColumns = board.columns.map((column, index) => {
-            column = Object.assign({}, column, { ticketOrder: newTicketOrdersForColumns[index] })
-            return column
-        })
-        //const filteredTasks = board.columns.map(column => column.tasks.map
-        // Finally we set the filteredBoard state to be a copy of the original except for the array of columns 
-        setFilteredBoard(prevState => ({ ...prevState, columns: filteredColumns }))
-
-        return filteredTicketList
-    }
-    console.log('filteredBoard', filteredBoard)
 
     return (
         <Grid
@@ -161,7 +128,7 @@ const BoardPage = ({ id, eventId }) => {
                 </Grid>
                 {selectedUser && (
                     <Grid item>
-                        <Button classes={{ root: classes.filterButton }} onClick={() => filterBoardByOption()}>Filter</Button>
+                        <Button classes={{ root: classes.filterButton }} onClick={() => filterBoardByOption(setFilteredBoard, board, selectedUser)}>Filter</Button>
                     </Grid>
                 )}
             </Grid>
